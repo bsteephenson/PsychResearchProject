01de8c245eb8d3a2867c93749f85030d8096a368
BSteephenson <benjaminsteephenson@gmail.com>
2014-05-14 19:50:44 -0400

    i don't know..

--- app/views/ManageParticipants.blade.php
+++ app/views/ManageParticipants.blade.php
@@ -1,76 +1,21 @@
 @extends('layouts.master')
 
 @section('title')
-  Manage Participants
+  The title
 @stop
 
 @section('css')
-	th{
-		width:100px;
-	}
+  body{
+    font-size:140px;
+  }
 @stop
 
 @section('javascript')
-	function populateTable(){
-		$('table').html('');
-		$('table').append('<tr><th>Name</th><th>Key</th><th>Compliance</th></tr>');
-		$.ajax({
-    	url : "all-participants",
-    	cache : false,
-    	success : function(arr){
-    		for (var i = arr.length - 1; i >= 0; i--) {
-    			$row = '<tr><td>' + arr[i].name + '</td><td>' + arr[i].key + '</td><td>' + arr[i].Compliance + '</td></tr>';
-
-    			$('table').append($row);
-    		};
-    	}
-    	});
-	}
-	$(document).ready(function(){
-		//populate table
-		populateTable();
-
-		//what to do on clicking the add button
-
-		$('#submit').on('click', function(){
-			//push to db
-			$.ajax({
-		    	type : "POST",
-		    	url : "create-participant",
-		    	data : {'name' : $('#name').val(), 'key' : $('#key').val()},
-		    	cache : false,
-		    	success : function(jsonStuff){
-		    		
-				}
-			});
-			//say success
-
-			//empty the form
-			$('#name').val('');
-			$('#key').val('');
-			//repopulate table
-
-			populateTable();
-
-			return false;
-		})
-
-		//what to do on editing of a cell
-	});
+  $(document).ready(function(){
+    alert("worketh?");
+  });
 @stop
 
 @section('body')
-
-	<h1>Manage the participants</h1>
-
-	<form id = "addParticipantForm">
-		<input id = "name" type = "text" placeholder = "Enter Name"></input>
-		<input id = "key" type = "text" placeholder = "Enter Key"></input>
-		<input id = "submit" type="submit" value = "submit">
-	</form>
-
-	<table>
-
-	</table>
-
+  The body
 @stop
