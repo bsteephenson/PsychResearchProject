cc4c388cc926bf0a75090e13faab9acf5e3e6e60
BSteephenson <benjaminsteephenson@gmail.com>
2014-05-13 23:08:21 -0400

    added created and updated fields to participants table

---
+++ app/database/migrations/2014_05_14_030259_add_created_and_updated_fields_to_participants.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCreatedAndUpdatedFieldsToParticipants extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('participants', function($table){
+			$table->timestamp('created_at');
+			$table->timestamp('updated_at');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
