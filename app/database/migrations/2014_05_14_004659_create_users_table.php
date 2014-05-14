33172027f96a79a0815a4767233b218bee2fd45a
BSteephenson <benjaminsteephenson@gmail.com>
2014-05-13 21:00:13 -0400

    Added the participants table to the database, corrected the database connection

---
+++ app/database/migrations/2014_05_14_004659_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('participants', function($table){
+			$table->increments('id');
+			$table->string('name');
+			$table->string('key');
+			$table->boolean('compliance')->default(FALSE);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
