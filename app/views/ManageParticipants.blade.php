7c44b4ec9b37b2f890aeac8b9139a7819bd510f1
BSteephenson <benjaminsteephenson@gmail.com>
2014-05-13 22:00:35 -0400

    Added basic html look and jquery ajax calls

--- app/views/ManageParticipants.blade.php
+++ app/views/ManageParticipants.blade.php
@@ -1,21 +1,70 @@
 @extends('layouts.master')
 
 @section('title')
-  The title
+  Manage Participants
 @stop
 
 @section('css')
-  body{
-    font-size:140px;
-  }
+	th{
+		width:100px;
+	}
 @stop
 
 @section('javascript')
-  $(document).ready(function(){
-    alert("worketh?");
-  });
+	function populateTable(){
+		$.ajax({
+    	url : "ManageParticipants/getParticipants",
+    	cache : false,
+    	success : function(jsonStuff){
+    		$('body').append(jsonStuff);
+    	}
+    	});
+	}
+	$(document).ready(function(){
+		//populate table
+		populateTable();
+
+		//what to do on clicking the add button
+
+		$('#submit').on('click', function(){
+			//push to db
+			$.ajax({
+		    	url : "ManageParticipants/createParticipant",
+		    	data : {name: $('#name').val(), key : $('#key').val()},
+		    	cache : false,
+		    	success : function(jsonStuff){
+		    		$('body').append(jsonStuff);
+				}
+			});
+			//say success
+
+			//empty the form
+
+			//repopulate table
+
+			return false;
+		})
+
+		//what to do on editing of a cell
+	});
 @stop
 
 @section('body')
-  The body
+
+	<h1>Manage the participants</h1>
+
+	<form id = "addParticipantForm">
+		<input id = "name" type = "text" placeholder = "Enter Name"></input>
+		<input id = "key" type = "text" placeholder = "Enter Key"></input>
+		<input id = "submit" type="submit" value = "submit">
+	</form>
+
+	<table>
+		<tr>
+			<th>Name</th>
+			<th>Key</th>
+			<th>Compliance</th>
+		</tr>
+	</table>
+
 @stop
