9061e6c290d1c2abb25667bf7be3a020e24ed44b
BSteephenson <benjaminsteephenson@gmail.com>
2014-05-15 08:44:00 -0400

    added an inventory forms controller

---
+++ app/controllers/InventoryController.php
@@ -0,0 +1,12 @@
+<?php
+
+class InventoryController extends BaseController {
+	//makes the page given the choser and the chosen
+	public postInventoryPage(){}
+
+	//creates a new row in db with the form data, redirects somewhere depending on other things..
+	public postInventoryForm(){}
+	
+}
+
+?>
