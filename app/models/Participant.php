e0cf4796d656d9a66d2612e2e4b70ea17491a6d5
BSteephenson <benjaminsteephenson@gmail.com>
2014-05-15 08:52:18 -0400

    added methods to check if the participant did his/her tasks

--- app/models/Participant.php
+++ app/models/Participant.php
@@ -2,6 +2,13 @@
 
 class Participant extends Eloquent{
 	protected $table = 'participants';
+
+	public didDoSelfInventory($id){
+		return PersonalityInventory::where('choser', '=' , $id)->where('chosen', '=', $id)->count();
+	}
+	public getNumberOfOtherInventories($id){
+		return PersonalityInventory::where('choser', '=' , $id)->where('chosen', '!=', $id)->count();
+	}
 }
 
 ?>
