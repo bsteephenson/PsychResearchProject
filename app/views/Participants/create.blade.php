7c6d3e8a33b236faffb244f9dc07643cc6871851
BSteephenson <benjaminsteephenson@gmail.com>
2014-05-14 20:00:46 -0400

    created CRUD views for participants

---
+++ app/views/Participants/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master')
+
+@section('title')
+  The title
+@stop
+
+@section('css')
+  body{
+    font-size:140px;
+  }
+@stop
+
+@section('javascript')
+  $(document).ready(function(){
+    //alert("worketh?");
+  });
+@stop
+
+@section('body')
+
+@stop
