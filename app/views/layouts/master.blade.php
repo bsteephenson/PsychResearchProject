9e654031c33e88c482efec559c98e550abf27862
BSteephenson <benjaminsteephenson@gmail.com>
2014-05-17 11:11:00 -0400

    added design to master template

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -20,15 +20,53 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     {{HTML::script("js/bootstrap.min.js")}}
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300' rel='stylesheet' type='text/css'>
     <style>
+      body{
+        background-color: #4D6684;
+        color: #BEC9D6;
+        font-family: 'Open Sans', 'sans-serif';
+        font-weight: 300;
+        font-size: 20px;
+      }
+      a{
+        color: #83AA30;
+      }
+      a:hover{
+        color: #83AA30;
+      }
+      div#content{
+        margin-left: 15%;
+        margin-right: 15%;
+        margin-top: 15px;
+
+      }
+      #mainHeader{
+        width:75%;
+        margin: 0 auto;
+        text-align: center;
+        font-size: 40px;
+        font-weight: 100;
+      }
+      #mainHeader>img{
+        margin: 0 auto;
+        margin-bottom: 10px;
+        display: block;
+        width: 100px;
+      }
       @yield('css')
     </style>
     <script type="text/javascript">
-      @yield('javascript')
+    $(document).ready(function(){
+      @yield('javascript')      
+    });
     </script>
 
   </head>
   <body>
-@yield('body')
+    <div id='content'>
+      @yield('body')
+    </div>
   </body>
 </html>
